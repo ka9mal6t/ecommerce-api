94ed7ff381c933b105811f0db798ad87bc2efb9f
ka9mal6t <vladimyr.kilko@gmail.com>
2026-09-17 21:39:48 +0200

    add orders models

---
+++ app/orders/models.py
@@ -0,0 +1,22 @@
+from datetime import datetime, timezone
+
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+
+from app.database import Base
+
+
+class Orders(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    status_id = Column(Integer, ForeignKey('status.id'))
+    created_at = Column(DateTime(timezone=True),
+                        default=datetime.now(timezone.utc))
+
+    user = relationship("Users", back_populates="orders")
+    order_items = relationship("OrderItems", back_populates="order")
+
+    def __str__(self):
+        return f"Order {self.id} - {self.status_id}"
