8955f4e386e65da4ef247cfd0ed81a326f6fde7d
ka9mal6t <vladimyr.kilko@gmail.com>
2026-09-18 10:27:18 +0200

    update main

--- app/main.py
+++ app/main.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
 from app.connection.router import router as connection_router
+from app.users.router import router as user_router
+import app.models as models
 
 app = FastAPI()
 
 app.include_router(connection_router)
+app.include_router(user_router)
 
 
 origins = ["*"]
