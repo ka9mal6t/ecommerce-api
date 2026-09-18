ae301f8be9e0a167fa83251120b7e68674777888
ka9mal6t <vladimyr.kilko@gmail.com>
2026-09-18 12:28:01 +0200

    add new routers

--- app/main.py
+++ app/main.py
@@ -3,12 +3,21 @@ from starlette.middleware.cors import CORSMiddleware
 
 from app.connection.router import router as connection_router
 from app.users.router import router as user_router
+from app.categories.router import router as category_router
+from app.products.router import router as product_router
+from app.orders.router import router as order_router
+from app.order_items.router import router as order_item_router
+
 import app.models as models
 
 app = FastAPI()
 
 app.include_router(connection_router)
 app.include_router(user_router)
+app.include_router(category_router)
+app.include_router(product_router)
+app.include_router(order_router)
+app.include_router(order_item_router)
 
 
 origins = ["*"]
