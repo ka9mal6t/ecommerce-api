6ed9511978ee5f88ca293c183e760ddafb8475cc
ka9mal6t <vladimyr.kilko@gmail.com>
2026-09-18 10:26:52 +0200

    add test closed connection

--- app/connection/router.py
+++ app/connection/router.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from app.users.dependencies import get_user
+
+from app.users.models import Users
 
 router = APIRouter(
     prefix="/connection",
@@ -12,3 +15,9 @@ router = APIRouter(
             )
 async def ping() -> str:
     return "pong"
+
+@router.get("/hello",
+            summary="Test connection",
+            description="This endpoint for closed test connection")
+async def stats_matches(current_user: Users = Depends(get_user)):
+     return f"hello {current_user.username}"
