747ab2c5afbb6bb267f1a1aae8da3477403d32bb
ka9mal6t <vladimyr.kilko@gmail.com>
2026-09-18 10:26:23 +0200

    add import models

---
+++ app/models/__init__.py
@@ -0,0 +1,7 @@
+from app.roles.models import Roles
+from app.users.models import Users
+from app.status.models import Status
+from app.categories.models import Categories
+from app.products.models import Products
+from app.orders.models import Orders
+from app.order_items.models import OrderItems
